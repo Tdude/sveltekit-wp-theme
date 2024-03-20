caa0aa1ec4a185fa7e6f3e481991f3266c658487
Tdude <tibbemail@gmail.com>
2024-03-20 22:13:39 +0100

    Fixing local Ionicon icons as SVGs

--- src/lib/components/FeatureSmall.svelte
+++ src/lib/components/FeatureSmall.svelte
@@ -2,14 +2,14 @@
 <script>
 export let title = '';
 export let excerpt = '';
-</script>
+export let iconName = '';
+import IconWrapper from './IconWrapper.svelte';
 
+</script>
 
 <div class="w-full md:w-1/2 lg:w-1/3 px-4">
   <div class="h-full p-8 text-center hover:bg-white rounded-md hover:shadow-xl transition duration-200">
-    <div class="inline-flex h-16 w-16 mb-6 mx-auto items-center justify-center text-white bg-green-500 rounded-lg">
-      <slot name="icon"></slot>
-    </div>
+      <IconWrapper iconName={iconName} />
     <h3 class="mb-4 text-xl md:text-2xl leading-tight font-bold">{title}</h3>
     <p class="text-slate-500 font-medium">{excerpt}</p>
   </div>
