8d7f26426826edf4f76085189705a9e8508eea2c
Tdude <tibbemail@gmail.com>
2024-03-26 16:28:54 +0100

    Add Icon component

--- src/lib/components/FeatureSmall.svelte
+++ src/lib/components/FeatureSmall.svelte
@@ -2,14 +2,14 @@
 <script>
 export let title = '';
 export let excerpt = '';
-export let iconName = '';
+export let icon = '';
 import IconWrapper from './IconWrapper.svelte';
 
 </script>
 
 <div class="w-full md:w-1/2 lg:w-1/3 px-4">
   <div class="h-full p-8 text-center hover:bg-white rounded-md hover:shadow-xl transition duration-200">
-      <IconWrapper iconName={iconName} />
+      <IconWrapper icon={icon} />
     <h3 class="mb-4 text-xl md:text-2xl leading-tight font-bold">{title}</h3>
     <p class="text-slate-500 font-medium">{excerpt}</p>
   </div>
