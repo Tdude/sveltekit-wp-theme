06016dc3481c02d23a176433a9044ef32e5f4136
Tdude <tibbemail@gmail.com>
2024-03-26 16:32:48 +0100

    Param names and cleanup

--- src/lib/components/NavMobile.svelte
+++ src/lib/components/NavMobile.svelte
@@ -1,22 +1,13 @@
 <!-- src/components/NavMobile.svelte -->
 <script>
-    import Logo from './Logo.svelte';
-    import NavLink from './NavLink.svelte';
-    let href = '';
-    let text = '';
+  import NavLink from './NavLink.svelte';
+  export let menuItems = [];
 </script>
 
-<nav class="flex justify-between p-6 px-4 bg-slate-900">
-  <div class="flex justify-between items-center w-full">
-    <div class="w-1/2 xl:w-1/3">
-      <Logo src="images/tryggve-logo-inv-green.svg" alt="" />
-    </div>
-    <div class="w-1/2 xl:w-1/3">
-      <ul class="hidden xl:flex xl:justify-center">
-        <NavLink href={href} text={text}/>
-      </ul>
-    </div>
-    <!-- Include login and register buttons here, similar to NavLink component usage -->
-  </div>
-  <!-- Include NavbarToggle button here -->
-</nav>
+<div class="mobile-nav bg-slate-800 p-4">
+  <ul>
+    {#each menuItems as {title, url}}
+      <NavLink href={url} text={title} />
+    {/each}
+  </ul>
+</div>
