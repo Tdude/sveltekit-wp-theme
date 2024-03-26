1194f86c64796020e6598dceaf7aa2aab5b256f6
Tdude <tibbemail@gmail.com>
2024-03-26 16:34:07 +0100

    Added params to component

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -1,31 +1,10 @@
 <!-- src/components/SiteHeader.svelte -->
 <script>
-    import Nav from './Nav.svelte';
-    import NavMobile from './NavMobile.svelte';
-    import NavbarToggle from './NavbarToggle.svelte';
-    import { writable } from 'svelte/store';
-    export let bgColorClass = ''; // Accept from the parent component
+    import Navbar from './Navbar.svelte';
+    export let bgColorClass = '';
 
-    const isMobileNavVisible = writable(false);
-
-    function toggleMobileNav() {
-        isMobileNavVisible.update(n => !n);
-    }
 </script>
 
-
-
-<div class="bg-gray">
-    <nav class="flex justify-between p-6 px-4 bg-slate-800" 
-        data-config-id="toggle-mobile" 
-        data-config-target=".navbar-menu" 
-        data-config-class="hidden">
-        <div class="flex items-center justify-between w-full">
-            <Nav bgColorClass={bgColorClass} />
-            <NavbarToggle on:click={toggleMobileNav} />
-        </div>
-    </nav>
-    {#if $isMobileNavVisible}
-        <NavMobile />
-    {/if}
+<div class={`bg-gray ${bgColorClass}`}>
+    <Navbar />
 </div>
