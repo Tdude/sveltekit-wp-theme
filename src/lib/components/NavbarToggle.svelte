6cbb93c192526af1fb7b406805240e4f5b732d1f
Tdude <tibbemail@gmail.com>
2024-03-26 16:33:19 +0100

    Added components for nav

--- src/lib/components/NavbarToggle.svelte
+++ src/lib/components/NavbarToggle.svelte
@@ -1,7 +1,14 @@
 <script>
-    export let toggle = () => {};
+  export let toggle = () => {};
+  let icon = 'menu-outline';
+  import IconSvg from './IconSvg.svelte';
+  import NavMobile from './NavMobile.svelte';
 </script>
 
 <button class="navbar-burger self-center xl:hidden" on:click={toggle}>
-  <i class="fa-solid fa-bars text-2xl text-slate-200 px-2 py-1 bg-slate-800 rounded" aria-hidden="true"></i>
+  <div class="inline-flex p-2 h-16 w-16 mb-6 mx-auto items-center justify-center text-white bg-slate-700 rounded-lg">
+    <IconSvg icon={icon} />
+    <NavMobile />
+  </div>
 </button>
+
